b72431ee7f0ee5389f52194962f71c517d22064b
Alistair Cooke <alistair.cooke77@gmail.com>
2022-07-18 16:47:20 +0100

    A little util for escaping Lucene queries

---
+++ AireLogic.LyricCount.Tests/MusicBrainz/EscapingLuceneQueries.cs
@@ -0,0 +1,44 @@
+namespace AireLogic.LyricCount.Tests.MusicBrainz;
+
+class EscapingLuceneQueries
+{
+    [Test]
+    public void ShouldPassThroughWhenNoSpecialCharacters()
+    {
+        LuceneEscape.Escape("hello world").ShouldBe("hello world");
+    }
+
+    [TestCase("+")]
+    [TestCase("-")]
+    [TestCase("&&")]
+    [TestCase("||")]
+    [TestCase("!")]
+    [TestCase("(")]
+    [TestCase(")")]
+    [TestCase("{")]
+    [TestCase("}")]
+    [TestCase("[")]
+    [TestCase("]")]
+    [TestCase("^")]
+    [TestCase("\"")]
+    [TestCase("~")]
+    [TestCase("*")]
+    [TestCase("?")]
+    [TestCase(":")]
+    [TestCase("/")]
+    [TestCase("\\")]
+    public void ShouldEscapeSpecialCharsWithABackslash(string special)
+    {
+        LuceneEscape.Escape($"hello{special}world")
+            .ShouldBe($"hello\\{special}world");
+    }
+
+    [TestCase("&")]
+    [TestCase("|")]
+    [TestCase(" ")]
+    public void ShouldNotEscapeThese(string notSpecial)
+    {
+        LuceneEscape.Escape($"hello{notSpecial}world")
+            .ShouldBe($"hello{notSpecial}world");
+    }
+}
---
+++ AireLogic.LyricCount.Tests/MusicBrainz/LuceneEscape.cs
@@ -0,0 +1,42 @@
+using System.Text;
+namespace AireLogic.LyricCount.Tests.MusicBrainz;
+
+static class LuceneEscape
+{
+    static readonly string[] Specials =
+    {
+        "+",
+        "-",
+        "&&",
+        "||",
+        "!",
+        "(",
+        ")",
+        "{",
+        "}",
+        "[",
+        "]",
+        "^",
+        "\"",
+        "~",
+        "*",
+        "?",
+        ":",
+        "/"
+    };
+
+    const char EscapeChar = '\\';
+
+    public static string Escape(string input)
+    {
+        var sb = new StringBuilder(input);
+
+        sb.Replace($"{EscapeChar}", $"{EscapeChar}{EscapeChar}");
+        foreach (var s in Specials)
+        {
+            sb.Replace(s, $"{EscapeChar}{s}");
+        };
+
+        return sb.ToString();
+    }
+}
