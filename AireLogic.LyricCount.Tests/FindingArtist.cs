b60b5bef5ac91b1de0ef05b1d24f11ebee12e261
Alistair Cooke <alistair.cooke77@gmail.com>
2022-08-02 13:22:28 +0100

    Class -> record: Artist

--- AireLogic.LyricCount.Tests/FindingArtist.cs
+++ AireLogic.LyricCount.Tests/FindingArtist.cs
@@ -43,9 +43,9 @@ class FindingArtist
         static readonly ArtistResponse ArtistResponse = new()
         {
             Artists = new[] {
-                new Artist { ID="11111111-1111-1111-1111-111111111111", Name="Found Artist 1" },
-                new Artist { ID="22222222-2222-2222-2222-222222222222", Name="Found Artist 2" },
-                new Artist { ID="33333333-3333-3333-3333-333333333333", Name="Found Artist 3" }
+                new Artist(ID:"11111111-1111-1111-1111-111111111111", Name:"Found Artist 1" ),
+                new Artist(ID:"22222222-2222-2222-2222-222222222222", Name:"Found Artist 2" ),
+                new Artist(ID:"33333333-3333-3333-3333-333333333333", Name:"Found Artist 3" )
             }
         };
 
